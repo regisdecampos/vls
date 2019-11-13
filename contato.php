0b44e8f26c97261604d6a9da095d84126053dbb6
alexcamargoapp <alexcamargo.app@gmail.com>
2019-11-13 13:01:58 -0300

    correcao links < e > do carousel do slider banner e alteracao da linha 36 contato.php

--- contato.php
+++ contato.php
@@ -17,12 +17,12 @@ define('GPWD', '');		// <-- Insira aqui a senha do seu GMail
 function smtpmailer($para, $de, $de_nome, $assunto, $corpo) { 
 	global $error;
 	$mail = new PHPMailer();
-	$mail->IsSMTP();		// Ativar SMTP
-	$mail->SMTPDebug = 0;		// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
-	$mail->SMTPAuth = true;		// Autenticação ativada
-	$mail->SMTPSecure = 'ssl';	// SSL REQUERIDO pelo GMail
+	$mail->IsSMTP();				// Ativar SMTP
+	$mail->SMTPDebug = 0;			// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
+	$mail->SMTPAuth = true;			// Autenticação ativada
+	$mail->SMTPSecure = 'ssl';		// SSL REQUERIDO pelo GMail
 	$mail->Host = 'smtp.gmail.com';	// SMTP utilizado
-	$mail->Port = 465;  		// A porta 587 deverá estar aberta em seu servidor
+	$mail->Port = 465;  			// A porta 587 deverá estar aberta em seu servidor
 	$mail->Username = GUSER;
 	$mail->Password = GPWD;
 	$mail->SetFrom($de, $de_nome);
@@ -33,7 +33,7 @@ function smtpmailer($para, $de, $de_nome, $assunto, $corpo) {
 		$error = 'Mail error: '.$mail->ErrorInfo; 
 		return false;
 	} else {
-		$error = 'Mensagem enviada!';
+		$error = '';
 		return true;
 	}
 }
@@ -41,7 +41,7 @@ function smtpmailer($para, $de, $de_nome, $assunto, $corpo) {
 // Insira abaixo o email que irá receber a mensagem, o email que irá enviar (o mesmo da variável GUSER), 
 // o nome do email que envia a mensagem, o Assunto da mensagem e por último a variável com o corpo do email.
 
- if (smtpmailer(GUSER, $Email, $Nome, 'Contato Site Maricleide', $Vai)) {
+ if (smtpmailer(GUSER, $Email, $Nome, 'Contato Site Voo Livre Sistemas', $Vai)) {
 
 	Header("location:contato_obrigado.html"); // Redireciona para uma página de obrigado.
 	echo $error;
